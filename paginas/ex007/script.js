1bb1866f7917ea2847025b49e3f313240286eec8
Felipe Gonçalves <114199894+felipgs@users.noreply.github.com>
2023-01-11 02:00:41 -0300

    exercício 007 finalizado

---
+++ paginas/ex007/script.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById("botao")
+btn.addEventListener("click", calcular)
+
+function calcular() {
+    let res = document.querySelector('div#res')
+    let nome = prompt("Qual é o nome do aluno?")
+    let n1 = Number(prompt(`Qual é a primeira nota de ${nome}?`))
+    let n2 = Number(prompt(`Além de ${n1}, qual foi a outra nota de ${nome}?`))
+    let media = (n1+n2) / 2
+    res.innerHTML = `<p>Calculando a média final de <mark>${nome}</mark></p>`
+    res.innerHTML += `<p>As notas obtidas foram <mark>${n1}</mark> e <mark>${n2}</mark> </p>`
+    res.innerHTML += `<p>A média final será <mark>${media}</mark>.</p>`
+}
+
+
