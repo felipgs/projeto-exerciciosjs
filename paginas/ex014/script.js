6c656a5646fb6c70147949bcf96e37d96cc68283
Felipe Gonçalves <114199894+felipgs@users.noreply.github.com>
2023-01-19 05:10:11 -0300

    exercício 014 finalizado

--- paginas/ex014/script.js
+++ paginas/ex014/script.js
@@ -1,6 +1,9 @@
 let res = document.querySelector('div#result')
 let btn = document.querySelector('button#botao')
 
-btn.addEventListener('click' () => {
-    console.log('Olá')
+btn.addEventListener('click', () => {
+    let now = new Date()
+    let semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"]
+    let mes = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+    res.innerHTML = `<p>O que eu recebi do sistema foi: Hoje é ${semana[now.getDay()]}, ${now.getDate()} de ${mes[now.getMonth()]} de ${now.getFullYear()}.</p>`
 })
